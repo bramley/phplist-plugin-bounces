ec8a18c5db9ce5f37955929588f37b786c0dc14a
MarcelvC <MarcelvC@users.noreply.github.com>
2015-05-29 19:45:18 +0200

    added German language file; added one item to English language array

---
+++ plugins/BounceStatisticsPlugin/lan/de.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+ * BounceStatisticsPlugin for phplist
+ * 
+ * This file is a part of BounceStatisticsPlugin.
+ *
+ * @category  phplist
+ * @package   BounceStatisticsPlugin
+ * @author    Duncan Cameron
+ * @copyright 2011-2012 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ * @version   SVN: $Id: en.php 1269 2013-04-29 15:04:49Z Duncan $
+ * @link      http://forums.phplist.com/viewtopic.php?f=7&t=37570
+ */
+
+/**
+ * This file contains the Germans translation of the English text
+ */
+$lan = array(
+    'plugin_title' => 'Bounce Statistics Plugin',
+    'domain' => 'Domain',
+    'reason' => 'Grund',
+    'users' => 'Abonnenten',
+    'bounce' => 'Bounce ID',
+    'bounce date' => 'Bounce-Datum',
+    'User email' => 'E-Mail-Adresse',
+    'message id' => 'ID Nach-<br />richt',
+    'domain name' => 'Domain Name',
+    'bounces' => 'Gesamtzahl Bounces',
+	'bounce count' => 'Anzahl Bounces',
+    'confirmed_heading' => 'Bestät.',
+    'not confirmed' => 'unbestätigt',
+    'blacklisted_heading' => 'gesp.',
+    'User is blacklisted' => 'Abonnent ist gesperrt (Blacklist)',
+    'email is blacklisted' => 'E-Mail ist gesperrt (Blacklist)',
+);
